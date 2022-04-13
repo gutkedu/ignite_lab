e416a3328e24eb32c33a1e18109f21a3f85afcc9
gutkedu <eduardo.pedogutkoski@gmail.com>
2022-04-12 23:11:11 -0300

    ✨ generating user acess token in front-end with auth0

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,25 @@
+import { getAccessToken, useUser } from "@auth0/nextjs-auth0"
+import { GetServerSideProps } from "next";
+
+export default function Home() {
+  const { user } = useUser()
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+
+      <a href="/api/auth/login">Login</a>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+
+  const token = getAccessToken(req, res);
+
+  return {
+    props: {},
+  }
+}
