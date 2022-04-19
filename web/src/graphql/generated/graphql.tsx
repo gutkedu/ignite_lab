b96507578f5942de0e07a3c7e2508c7b274c6bcb
gutkedu <eduardo.pedogutkoski@gmail.com>
2022-04-19 00:31:23 -0300

    feat: front-end integration with backend ready

--- web/src/graphql/generated/graphql.tsx
+++ web/src/graphql/generated/graphql.tsx
@@ -112,12 +112,51 @@ export type User = {
   purchases: Purchase;
 };
 
+export type MeQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type MeQuery = { __typename?: 'Query', me: { __typename?: 'User', authUserId: string } };
+
 export type GetProductsQueryVariables = Exact<{ [key: string]: never; }>;
 
 
 export type GetProductsQuery = { __typename?: 'Query', products: Array<{ __typename?: 'Product', id: string, title: string }> };
 
 
+export const MeDocument = gql`
+    query Me {
+  me {
+    authUserId
+  }
+}
+    `;
+
+/**
+ * __useMeQuery__
+ *
+ * To run a query within a React component, call `useMeQuery` and pass it any options that fit your needs.
+ * When your component renders, `useMeQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useMeQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useMeQuery(baseOptions?: Apollo.QueryHookOptions<MeQuery, MeQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<MeQuery, MeQueryVariables>(MeDocument, options);
+      }
+export function useMeLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<MeQuery, MeQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<MeQuery, MeQueryVariables>(MeDocument, options);
+        }
+export type MeQueryHookResult = ReturnType<typeof useMeQuery>;
+export type MeLazyQueryHookResult = ReturnType<typeof useMeLazyQuery>;
+export type MeQueryResult = Apollo.QueryResult<MeQuery, MeQueryVariables>;
 export const GetProductsDocument = gql`
     query GetProducts {
   products {
@@ -143,13 +182,13 @@ export const GetProductsDocument = gql`
  * });
  */
 export function useGetProductsQuery(baseOptions?: Apollo.QueryHookOptions<GetProductsQuery, GetProductsQueryVariables>) {
-  const options = { ...defaultOptions, ...baseOptions }
-  return Apollo.useQuery<GetProductsQuery, GetProductsQueryVariables>(GetProductsDocument, options);
-}
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetProductsQuery, GetProductsQueryVariables>(GetProductsDocument, options);
+      }
 export function useGetProductsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetProductsQuery, GetProductsQueryVariables>) {
-  const options = { ...defaultOptions, ...baseOptions }
-  return Apollo.useLazyQuery<GetProductsQuery, GetProductsQueryVariables>(GetProductsDocument, options);
-}
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetProductsQuery, GetProductsQueryVariables>(GetProductsDocument, options);
+        }
 export type GetProductsQueryHookResult = ReturnType<typeof useGetProductsQuery>;
 export type GetProductsLazyQueryHookResult = ReturnType<typeof useGetProductsLazyQuery>;
 export type GetProductsQueryResult = Apollo.QueryResult<GetProductsQuery, GetProductsQueryVariables>;
--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,16 +1,20 @@
-import { getAccessToken, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0"
-import { useGetProductsQuery } from "../../graphql/generated/graphql"
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0"
+import { useMeQuery } from "../../graphql/generated/graphql"
+import { getServerPageGetProducts, ssrGetProducts } from "../../graphql/generated/page"
 import { withApollo } from "../../lib/withApollo"
 
-function Home() {
+function Home({ data }) {
   const { user } = useUser()
-  const { data, loading, error } = useGetProductsQuery()
+  const { data: me } = useMeQuery()
 
   return (
-    <div>
+    <div className="text-violet-500">
       <h1>Hello World</h1>
       <pre>
-        {JSON.stringify(data, null, 2)}
+        ok :{JSON.stringify(me, null, 2)}
+      </pre>
+      <pre>
+        {/*JSON.stringify(data.products, null, 2)*/}
       </pre>
       <pre>
         {JSON.stringify(user, null, 2)}
@@ -20,12 +24,12 @@ function Home() {
 }
 
 export const getServerSideProps = withPageAuthRequired({
-  getServerSideProps: async ({ req, res }) => {
-    console.log(getAccessToken(req, res));
-    return {
-      props: {}
-    }
+  getServerSideProps: async (ctx) => {
+    //return getServerPageGetProducts(null, ctx)
+    return { props: {} }
   }
 });
 
-export default withApollo(Home);
+export default withApollo(
+  ssrGetProducts.withPage()(Home)
+);
