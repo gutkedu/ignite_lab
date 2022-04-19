5e26edefbb7c1300f3894e8929c60505bfce101d
gutkedu <eduardo.pedogutkoski@gmail.com>
2022-04-18 23:27:49 -0300

    ✨ feat: graphql query generator

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,11 +1,17 @@
 import { getAccessToken, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0"
+import { useGetProductsQuery } from "../../graphql/generated/graphql"
+import { withApollo } from "../../lib/withApollo"
 
-export default function Home() {
+function Home() {
   const { user } = useUser()
+  const { data, loading, error } = useGetProductsQuery()
 
   return (
     <div>
       <h1>Hello World</h1>
+      <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre>
       <pre>
         {JSON.stringify(user, null, 2)}
       </pre>
@@ -21,3 +27,5 @@ export const getServerSideProps = withPageAuthRequired({
     }
   }
 });
+
+export default withApollo(Home);
